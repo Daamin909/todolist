0f7e24a1547367b4f0d719c3d8bebbd2b91a33c8
Daamin909 <ashai.daamin@gmail.com>
2025-09-13 11:18:18 +0530

    simplify code for da noobies

--- App.js
+++ App.js
@@ -5,19 +5,17 @@ export default function App() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
 
-  const addTask = () => {
-    if (task.length === 0) {
-      return;
-    }
+  function addTask() {
+    if (task.length === 0) return;
     setTasks([...tasks, task]);
     setTask("");
-  };
+  }
 
-  const deleteTask = (index) => {
+  function deleteTask(index) {
     const copy = [...tasks];
     copy.splice(index, 1);
     setTasks(copy);
-  };
+  }
 
   return (
     <View style={styles.container}>
@@ -35,33 +33,30 @@ export default function App() {
         <Button title="Add" onPress={addTask} />
       </View>
 
-      <View style={styles.list}>
-        {tasks.map((t, i) => (
-          <View key={i} style={styles.taskRow}>
-            <Text style={styles.taskText}>{t}</Text>
-            <Button title="✓" onPress={() => deleteTask(i)} />
-          </View>
-        ))}
-      </View>
+      {tasks.map((t, i) => (
+        <View key={i} style={styles.taskRow}>
+          <Text style={styles.taskText}>{t}</Text>
+          <Button title="✓" onPress={() => deleteTask(i)} />
+        </View>
+      ))}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  navbar: {
-    backgroundColor: "#072434",
-    padding: 10,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#d4e8f3",
+  container: { flex: 1, backgroundColor: "#d4e8f3" },
+  navbar: { backgroundColor: "#072434", padding: 10 },
+  heading: {
+    fontSize: 40,
+    textAlign: "center",
+    fontWeight: "900",
+    fontFamily: "monospace",
+    color: "#e3eef4",
   },
   inputRow: {
     flexDirection: "row",
-    marginBottom: 20,
     alignItems: "center",
-    paddingHorizontal: 20,
-    paddingVertical: 20,
+    padding: 20,
   },
   input: {
     flex: 1,
@@ -73,29 +68,16 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     fontSize: 22,
   },
-  list: {
-    marginTop: 10,
-  },
   taskRow: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     paddingVertical: 8,
     paddingHorizontal: 12,
-    marginBottom: 8,
+    marginVertical: 4,
+    marginHorizontal: 15,
     backgroundColor: "#f2f2f2",
     borderRadius: 5,
-    marginVertical: 2,
-    marginHorizontal: 15,
   },
-  taskText: {
-    fontSize: 16,
-  },
-  heading: {
-    fontSize: 40,
-    textAlign: "center",
-    fontWeight: 900,
-    fontFamily: "monospace",
-    color: "#e3eef4",
-  },
+  taskText: { fontSize: 16 },
 });
